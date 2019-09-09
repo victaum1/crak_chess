65dc6c380c1104d4d43fc9b2e19711d4677bc50b
Victor H. Manotas G <victorma31@gmail.com>
2019-09-09 17:21:11 -0500

    Updating the square show

--- Squares.hs
+++ Squares.hs
@@ -1,5 +1,7 @@
 module Squares (File(..), Rank(..), Square(..)) where
 
+import Data.Maybe
+
 data File = A_F | B_F | C_F | D_F | E_F | F_F | G_F | H_F
   deriving (Show,Eq,Ord)
 
@@ -11,6 +13,22 @@ data Square = Square {
                        , squareRank :: Rank
                       } deriving Eq
 
+chrFileList = "ABCDEFGH"
+chrRankList = "12345678"
+
+fileList = [A_F,B_F,C_F,D_F,E_F,F_F,G_F,H_F]
+rankList = [R1,R2,R3,R4,R5,R6,R7,R8]
+
+chr2File' = zip fileList chrFileList
+
+chr2Rank' = zip rankList chrRankList
+
+showFile :: File -> Char
+showFile f = fromJust (lookup f chr2File')
+
+showRank :: Rank -> Char
+showRank r = fromJust (lookup r chr2Rank')
+
 instance Show Square where
-  show (Square a b) =  "("++ show a ++","++ show b ++ ")"
+  show (Square a b) =  [showFile a] ++ [showRank b] 
 
