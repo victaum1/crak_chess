56a4c1de4cdbe05f9059881798b73bfdd0ff8390
Victor H. Manotas G <victorma31@gmail.com>
2019-09-09 18:29:17 -0500

    readSquare...

--- Squares.hs
+++ Squares.hs
@@ -1,5 +1,6 @@
 module Squares (File(..), Rank(..), Square(..)) where
 
+import Data.Char (toUpper)
 import Data.Maybe
 
 data File = A_F | B_F | C_F | D_F | E_F | F_F | G_F | H_F
@@ -19,8 +20,10 @@ chrRankList = "12345678"
 fileList = [A_F,B_F,C_F,D_F,E_F,F_F,G_F,H_F]
 rankList = [R1,R2,R3,R4,R5,R6,R7,R8]
 
+chr2File = zip chrFileList fileList
 chr2File' = zip fileList chrFileList
 
+chr2Rank = zip chrRankList rankList
 chr2Rank' = zip rankList chrRankList
 
 showFile :: File -> Char
@@ -31,4 +34,21 @@ showRank r = fromJust (lookup r chr2Rank')
 
 instance Show Square where
   show (Square a b) =  [showFile a] ++ [showRank b] 
+
+readCFile :: Char -> Maybe File
+readCFile c = lookup (toUpper c) chr2File 
+
+readRank :: Char -> Maybe Rank
+readRank c = lookup (toUpper c) chr2Rank 
+
+makeSquare :: Char -> Char -> Maybe Square
+makeSquare c1 c2 = if (isNothing (mkF c1) || isNothing (mkR c2))
+  then Nothing  else Just (Square (fromJust (mkF c1))
+  (fromJust (mkR c2)))
+  where mkF = readCFile
+        mkR = readRank
+
+readSquare :: String -> Maybe Square
+readSquare str = if (length str < 2)  then Nothing else makeSquare  (str!!0)
+ (str!!1)
 
