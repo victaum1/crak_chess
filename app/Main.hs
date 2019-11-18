540c133f2e69906ca99a795019539b1855c15279
Victor H. Manotas G <victorma31@gmail.com>
2019-11-17 19:29:27 -0500

    Implementing help command in cli.

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,36 @@
 module Main where
 
--- import Data.Maybe
 import System.IO
 import System.Console.Haskeline
 import Adapter
 
+version = "0.0.10.6"
+help_str = unlines [
+  "play  - Engine thinks and plays for the current turn."
+  ,"stop - Engine Stops."
+  ,"st n - Sets the time per move in seconds."
+  ,"sd n - Sets the deph of searching."
+  ,"undo - Undo a move."
+  ,"new - New game."
+  ,"dump - Dumps the board."
+  ,"quit - Exits the engine."
+  ,"xboard - Switch to xboard protocol."
+  ,"uci - Switch to uci protocol."
+  ]
+
 main_loop = do
-              maybeline <- getInputLine "> "
+              maybeline <- getInputLine "crak> "
               case maybeline of
                 Nothing       -> return () -- EOF / ctrl-d
                 Just "quit"   -> return ()
                 Just "xboard" -> xboard_loop
                 Just "uci"    -> uci_loop
+                Just "help" -> do
+                  outputStrLn help_str
+                  main_loop 
                 Just line     -> do
-                   outputStrLn $ "Error (unknown command) : " ++ line
-                   main_loop
+                  outputStrLn $ "Error (unknown command) : " ++ line
+                  main_loop
 
 main :: IO ()
 main = do
@@ -23,5 +39,8 @@ main = do
        where
          entry :: InputT IO ()
          entry = do
-           outputStrLn "Craken 0.0.10.x by V. Manotas"
+           outputStrLn $ "Craken " ++ version ++ " by V. Manotas."
+           outputStrLn "x/x/2019."
+           outputStrLn ""
+           outputStrLn "'help' show usage."
            main_loop
--- update_ver.sh
+++ update_ver.sh
@@ -3,26 +3,28 @@
 set -e
 
 VER_FILE=VERSION
-FILES_2_UPDATE_EN="crak-chess.cabal README.md"
-FILES_2_UPDATE_SP="LEEME.md"
+FILE_0="app/Main.hs"
+FILE_1="crak-chess.cabal"
+FILE_2="README.md"
+FILE_3="LEEME.md"
+FILES="${FILE_0} ${FILE_1} ${FILE_2} ${FILE_3}" 
 
-FILES="${FILES_2_UPDATE_EN} ${FILES_2_UPDATE_SP}"
-
-echo "Updating version in $FILES ..."
+echo "Updating version in: ${FILES}"
 
 read VER < $VER_FILE
 
-for item in $FILES; do
-	sed -i -e "s/version: [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+/version: ${VER}/g" $item
-done
+sed -i -e \
+"s/version = [\"][0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+[\"]/version = \"${VER}\"/g" \
+  $FILE_0
 
-for item in $FILES; do
-	sed -i -e "s/version [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+/version ${VER}/g" $item
-done
+sed -i -e "s/version: [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+/version: ${VER}/g" \
+	$FILE_1
 
-for item in $FILES; do
-	sed -i -e "s/versión [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+/versión ${VER}/g" $item
-done
+sed -i -e "s/version [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+/version ${VER}/g" \
+	$FILE_2
+
+sed -i -e "s/versión [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+/versión ${VER}/g" \
+	$FILE_3
 
 echo "done!"
 
