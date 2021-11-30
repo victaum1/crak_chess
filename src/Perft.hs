337c0e7771d8651d3208eec17d04a933ba2e86a5
Victor H. Manotas G <victorma31@gmail.com>
2021-11-30 17:12:01 -0500

    Bugfixing make-move with Perft

--- src/Perft.hs
+++ src/Perft.hs
@@ -4,21 +4,16 @@ module Perft where
 -- import Valid
 -- import Play
 -- import Data.Maybe
--- import Moves
--- import Parsing
 
 
 import Game ( Game, pGame )
 import Valid ( genValidMoves )
 import Play ( makeMove )
 import Data.Maybe ( mapMaybe, fromJust, isNothing )
-import Moves ( Move )
-import Parsing ( parse )
 
 perft :: Game -> Int -> Int
 perft p d | d == 0 = 1
           | otherwise = sum $ map (`perft` (d-1)) sucPos
   where moves = genValidMoves p
         sucPos = mapMaybe (`makeMove` p) moves
-
 
--- tests/u-play.hs
+++ tests/u-play.hs
@@ -31,9 +31,9 @@ main =
   inputs_ <- readFile(fixtures ++ inputs)
   let slines = noHeadLines inputs_
   let triples = map (splitOn ';') slines
-  let moves = map genMoves $ map head triples
-  let inp_games = map genGames $ map (!!1) triples
-  let spec_games = map Just $ map genGames $ map (!!2) triples
+  let moves = map (genMoves . head) triples
+  let inp_games = map (genGames . (!!1)) triples
+  let spec_games = map ((Just . genGames) . (!!2)) triples
   let out_games = genOutGames moves inp_games
   let pre_tests = genTest spec_games out_games
   let tests = TestList $ map TestCase pre_tests
